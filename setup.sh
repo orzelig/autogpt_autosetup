157f5ac3fbf017ee388394cc25235692b733eb73
eran <eran@AutoGPT.myguest.virtualbox.org>
2023-06-02 13:00:35 +0300

    added default option to build docker from offical repo

--- setup.sh
+++ setup.sh
@@ -12,27 +12,6 @@ echo "Creating folder..."
 mkdir Auto-GPT
 cd Auto-GPT
 
-echo "Create docker-compose.yml..."
-cat << EOF > docker-compose.yml
-version: "3.9"
-services:
-  auto-gpt:
-    image: significantgravitas/auto-gpt
-    depends_on:
-      - redis
-    env_file:
-      - .env
-    environment:
-      MEMORY_BACKEND: \${MEMORY_BACKEND:-redis}
-      REDIS_HOST: \${REDIS_HOST:-redis}
-    profiles: ["exclude-from-up"]
-    volumes:
-      - ./auto_gpt_workspace:/app/autogpt/auto_gpt_workspace
-      - ./data:/app/data
-      - ./logs:/app/logs
-  redis:
-    image: "redis/redis-stack-server:latest"
-EOF
 
 # Ask the user for their API key
 echo "Please enter your OpenAI API key:"
@@ -44,8 +23,50 @@ echo "OPENAI_API_KEY=$OPENAI_API_KEY" > .env
 # Notify the user that the file was created
 echo "The .env file has been created with your API key."
 
-echo "pull docker..."
-sudo docker pull significantgravitas/auto-gpt
+
+while true; do
+  # Ask the user for input
+  echo "Use official repo docker-compose.yml (takes longer time) ? [Y/n]"
+
+  # Read the user's answer
+  read answer
+  
+  # If the answer is empty, set it to "yes"
+  if [[ -z $answer ]]; then
+    answer="y"
+  fi
+
+  # Process the answer
+  case $answer in
+    [yY] | [yY][eE][sS] )
+      echo "You chose to use the official repo docker-compose.yml"
+      git init
+      git remote add origin https://github.com/Significant-Gravitas/Auto-GPT.git
+      git fetch origin
+      git checkout -b stable --track origin/stable
+      
+      echo "build AutoGPT docker"
+      sudo docker-compose build auto-gpt
+      break # Exit the loop
+      ;;
+      
+    [nN] | [nN][oO] )
+      echo "Using defualt docker-compose.yml instead"
+      cp ../default-docker-compose.yml ./docker-compose.yml
+      
+      echo "pull docker..."
+      sudo docker pull significantgravitas/auto-gpt
+
+      break # Exit the loop
+      ;;
+
+    # If the user enters anything else, ask them again
+    * )
+      echo "Invalid input. Please answer with yes or no (y/n)."
+      ;;
+  esac
+done
+
 
 echo "starting AutoGPT..."
 sudo docker-compose run --rm auto-gpt
