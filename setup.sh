a778abe7095091fee3e1d1da6402ddbb1b2ba7f1
orzelig <or@zelig.me>
2023-06-02 00:47:07 -0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -2,36 +2,49 @@
 
 # This script sets up and runs Auto-GPT in a Docker container.
 
-# Start with some debugging information
 echo "Starting Auto-GPT setup script..."
-
-# Check if the user is in the docker group
-if ! id -nG "$USER" | grep -qw docker; then
-    echo "Adding user to the docker group..."
-    sudo usermod -aG docker $USER
-fi
-
-# Re-evaluating group membership
-echo "Re-evaluating group membership..."
-sudo su - $USER
-
-# Cloning the Auto-GPT repository
-echo "Cloning the Auto-GPT repository..."
-git clone -b stable https://github.com/Significant-Gravitas/Auto-GPT.git
+sudo apt install -y docker.io
+echo "Starting Auto-GPT setup script..."
+sudo apt install docker-compose
+echo "Starting Auto-GPT setup script..."
+docker -v
+echo "Starting Auto-GPT setup script..."
+mkdir Auto-GPT
 cd Auto-GPT
 
-# Ask the user for their OpenAI API key
-echo "Enter your OpenAI API key: "
+echo "Create docker-compose.yml..."
+cat << EOF > docker-compose.yml
+version: "3.9"
+services:
+  auto-gpt:
+    image: significantgravitas/auto-gpt
+    depends_on:
+      - redis
+    env_file:
+      - .env
+    environment:
+      MEMORY_BACKEND: \${MEMORY_BACKEND:-redis}
+      REDIS_HOST: \${REDIS_HOST:-redis}
+    profiles: ["exclude-from-up"]
+    volumes:
+      - ./auto_gpt_workspace:/app/autogpt/auto_gpt_workspace
+      - ./data:/app/data
+      - ./logs:/app/logs
+  redis:
+    image: "redis/redis-stack-server:latest"
+EOF
+
+# Ask the user for their API key
+echo "Please enter your OpenAI API key:"
 read OPENAI_API_KEY
 
-# Replacing the placeholder with the user's OpenAI API key in the .env file
-echo "Replacing the placeholder with your OpenAI API key in the .env file..."
-sed -i "s/your-openai-key/${OPENAI_API_KEY}/g" .env
+# Create or overwrite the .env file
+echo "OPENAI_API_KEY=$OPENAI_API_KEY" > .env
 
-# Pulling the latest Docker image
-echo "Pulling the latest Docker image..."
-docker pull significantgravitas/auto-gpt
+# Notify the user that the file was created
+echo "The .env file has been created with your API key."
 
-# Running the Docker image
-echo "Running the Docker image..."
-docker-compose run --rm auto-gpt
+sudo docker pull significantgravitas/auto-gpt
+
+sudo docker-compose run --rm auto-gpt
+
